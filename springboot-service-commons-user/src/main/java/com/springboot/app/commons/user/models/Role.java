5936b18e877eb430172f3353698c4c374d7f7559
fraaan <castillofranciscodaniel@gmail.com>
2020-05-15 05:15:41 -0300

    service user funcionando con zuul

---
+++ springboot-service-commons-user/src/main/java/com/springboot/app/commons/user/models/Role.java
@@ -0,0 +1,24 @@
+package com.springboot.app.commons.user.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.springboot.app.commons.models.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role extends Model implements Serializable {
+
+	private static final long serialVersionUID = -5840653378609792375L;
+
+	@Column(unique = true, length = 20)
+	private String name;
+}
---
+++ springboot-service-commons-user/src/test/java/com/springboot/app/commons/user/SpringbootServiceCommonsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.app.commons.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceCommonsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
