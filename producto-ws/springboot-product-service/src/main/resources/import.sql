1e1ff0ed5f0ec64c3d9be9ddcf994ba318c6c5ca
fraaan <castillofranciscodaniel@gmail.com>
2020-05-17 14:23:37 -0300

    nuevo encarpetado

--- producto-ws/springboot-product-service/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO products(name, price) VALUES ('arroz', 100);
-INSERT INTO products(name, price) VALUES ('harina', 100);
-INSERT INTO products(name, price) VALUES ('papa', 50);
-INSERT INTO products(name, price) VALUES ('banana', 100);
