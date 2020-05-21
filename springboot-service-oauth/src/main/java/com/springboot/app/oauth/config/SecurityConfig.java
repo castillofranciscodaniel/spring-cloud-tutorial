abbe6611f85853d92974587b3294c03603fb4bb6
fraaan <castillofranciscodaniel@gmail.com>
2020-05-21 03:23:58 -0300

    manejadopr de eventos

--- springboot-service-oauth/src/main/java/com/springboot/app/oauth/config/SecurityConfig.java
+++ springboot-service-oauth/src/main/java/com/springboot/app/oauth/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.springboot.app.oauth.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,6 +16,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserDetailsService customUserDetailsService;
 	
+	@Autowired
+	private AuthenticationEventPublisher eventPublisher;
+	
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -22,9 +26,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	@Autowired
-	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-		authenticationManagerBuilder.userDetailsService(this.customUserDetailsService)
-				.passwordEncoder(passwordEncoder());
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(this.customUserDetailsService)
+				.passwordEncoder(passwordEncoder())
+				.and().authenticationEventPublisher(this.eventPublisher);
 	}
 
 	@Bean
