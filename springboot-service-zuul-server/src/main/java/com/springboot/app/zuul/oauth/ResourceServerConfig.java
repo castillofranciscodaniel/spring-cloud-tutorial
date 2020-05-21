79f4a9833a36211d822beed1be798a27d0d39c57
fraaan <castillofranciscodaniel@gmail.com>
2020-05-21 02:55:32 -0300

    cors configurado

--- springboot-service-zuul-server/src/main/java/com/springboot/app/zuul/oauth/ResourceServerConfig.java
+++ springboot-service-zuul-server/src/main/java/com/springboot/app/zuul/oauth/ResourceServerConfig.java
@@ -1,9 +1,13 @@
 package com.springboot.app.zuul.oauth;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -11,6 +15,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @RefreshScope
 @Configuration
@@ -18,8 +26,8 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Value("${config.security.oauth.jwt.key}")
-	private String  jwtKey;
-	
+	private String jwtKey;
+
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.tokenStore(this.jwtTokenStore());
@@ -28,11 +36,35 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		// tienen prioridad por el orden ...
-		http.authorizeRequests()
-				.antMatchers("api/security/oauth/token").permitAll()
+		http.authorizeRequests().antMatchers("/api/security/oauth/token").permitAll()
 				.antMatchers(HttpMethod.GET, "/api/products", "/api/items", "/api/users").permitAll()
-				.antMatchers(HttpMethod.GET, "/api/products/{id}", "/api/users/{id}").hasAnyRole("ADMIN","USER")
-				.antMatchers("/api/items/**", "/api/items/**", "/api/users/**").hasRole("ADMIN");
+				.antMatchers(HttpMethod.GET, "/api/products/{id}", "/api/users/{id}").hasAnyRole("ADMIN", "USER")
+				.antMatchers("/api/items/**", "/api/items/**", "/api/users/**").hasRole("ADMIN").anyRequest()
+				.authenticated().and().cors().configurationSource(corsConfigurationSource());
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		// * --> cualquier origen
+		corsConfig.addAllowedOrigin("*");
+		// corsConfig.addAllowedOrigin("http://localhost:4200");
+		// corsConfig.setAllowedOrigins(Arrays.asList("", ""));
+		corsConfig.setAllowedMethods(Arrays.asList("GET", "PUT", "POST", "DELETE", "OPTIONS"));
+		corsConfig.setAllowCredentials(true);
+		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+				source.registerCorsConfiguration("/**", corsConfig);;
+				
+		return source;
+	}
+	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(this.corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
 	}
 
 	@Bean
@@ -44,7 +76,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
 		tokenConverter.setSigningKey(this.jwtKey);
-		
+
 		return tokenConverter;
 	}
 }
